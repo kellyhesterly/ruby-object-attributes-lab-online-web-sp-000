90d4eef967be861fd5d0e67effb2ec79da572056
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-04 17:48:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,27 +1,11 @@
 class Dog
 
-def initalize(name)
+  attr_accessor :name, :breed
+
+def initialize
   @name = name
+  @breed = breed
 end
 
-def name
-  @name
-end
-
-def name=(new_name)
-    @name = new_name
-  end
-
-def initalize(breed)
-    @breed = breed
-end
-
-def breed
-  @breed
-end
-
-def breed=(new_breed)
-  @breed = new_breed
-end
 
 end
